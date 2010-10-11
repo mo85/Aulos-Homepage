44be72006f6acf25ee6c6cdbceb5bca567bdfc08
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-11 14:15:30 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aulos::Application.config.secret_token = 'c52648505cd55920f88926ac0f5215fe3a33529866335617ff6cc429dea97a2551543f10b8b75851ac25ca867e2617f933a086653794a01fc176b91bb3ca13e8'
