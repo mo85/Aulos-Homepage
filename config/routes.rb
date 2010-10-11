d6cde1dd62a52095aac5a7fb4d1faecf842296b9
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-11 15:32:27 +0200

    some things

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Aulos::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "root#index"
 
   # See how all your routes lay out with "rake routes"
 
