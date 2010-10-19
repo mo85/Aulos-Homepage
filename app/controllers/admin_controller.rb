c6d5dddb1ee2c8f7ac0dd0f5f8f5e0469d0879e0
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-19 21:33:48 +0200

    log in mechanism completed

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,21 @@
+class AdminController < ApplicationController
+  
+  def login
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])
+      if user
+        session[:user_id] = user.id
+        redirect_to(:controller => "root", :action => "index")
+      else
+        flash.now[:notice] = "Invalid user/password combination"
+      end
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to(:controller => 'root')
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  protected
+    def logged_in?
+      session[:user_id]
+    end
+    helper_method :logged_in?
 end
