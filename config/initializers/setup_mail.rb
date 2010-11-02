18639725e0f919118410f18eca44d1f786d3ce1c
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-11-02 14:04:03 +0100

    mailer for heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,14 +1,14 @@
 require "development_mail_interceptor"
 
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "aulos.ch",
-  :user_name            => "aulos.orchester",
-  :password             => "secret1234",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "aulos.ch",
+#   :user_name            => "aulos.orchester",
+#   :password             => "secret1234",
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 
