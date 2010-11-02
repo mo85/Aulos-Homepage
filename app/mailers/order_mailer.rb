194aafbbda48158af2701310565e861937645a09
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-11-02 14:05:58 +0100

    new cofig for orders

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,5 @@
 class OrderMailer < ActionMailer::Base
-  #default :from => "no-reply@aulos.ch"
-  default :from => "aulos.orchester@gmail.com"
+  default :from => "no-reply@aulos.ch"
   
   def order_confirmation
     mail(:to => "mark.odermatt@gmail.com", :subject => "Bestell Bestaetigung")
